d387c65c9ce91a9c26f22ac4d78c5ad993e8cb76
Wolfy <s.leheup@gmail.com>
2017-04-09 16:21:45 +0200

    Front done without specifical design

--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -1,4 +1,42 @@
 angular.module('app')
     .controller('MainController', function($scope) {
-      /* Here is your main controller */
+
+        $scope.presentList = [];
+        $scope.futureList = [];
+
+        $scope.addNow = function() {
+            $scope.presentList.push($scope.now);
+            $scope.now = '';
+        };
+
+        $scope.editNow = function(id) {
+          $scope.editMe = true;
+        };
+
+        $scope.editNowDone = function(id, editValue){
+          $scope.presentList.splice(id, 1, editValue);
+          $scope.editMe = false;
+        };
+
+        $scope.deleteNow = function(id) {
+            $scope.presentList.splice(this.$index, 1);
+        };
+
+        $scope.addLater = function() {
+            $scope.futureList.push($scope.later);
+            $scope.later = '';
+        };
+
+        $scope.editLater = function(id) {
+          $scope.editMe = true;
+        };
+
+        $scope.editLaterDone = function(id, editValue){
+          $scope.futureList.splice(id, 1, editValue);
+          $scope.editMe = false;
+        };
+
+        $scope.deleteLater = function(id) {
+            $scope.futureList.splice(id, 1);
+        };
     });
